bade7ee19bf8a4d7480de0bcedfaf4fb055498a7
Juan Carlos Rojas <jc.rojas1@uniandes.edu.co>
2017-06-01 08:38:45 -0500

    Al usar typeof puedo saber el tipo de dato de una variable.

---
+++ recursividad/funcionresursiva.js
@@ -0,0 +1,22 @@
+/**
+ * Created by jcrojas on 03/05/2017.
+ */
+/*Concepto de resursividad*/
+var contador;
+var total =1;
+var exponente;
+contador = 1;
+var power = function (base,valor){
+    if (contador == 1){
+        exponente = valor;
+        total = base;
+    }
+    if (contador < exponente) {
+        total = base * total;
+        contador = contador + 1;
+        total = power(base,total);
+    }
+    return total;
+}
+
+console.log(power(2,4));
