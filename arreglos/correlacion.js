30cdb1e40847cd4141d3f2ab667a6d7b3973f725
Juan Carlos Rojas <jc.rojas1@uniandes.edu.co>
2017-06-05 11:28:20 -0500

    Se cuentan las veces en que una variable es true o false.

--- arreglos/correlacion.js
+++ arreglos/correlacion.js
@@ -3,15 +3,20 @@
  */
 
 function hasEvent(event, entry) {
-    return entry.events.indexOf(event) != -1;
+    return entry.events.indexOf(event) !== -1;
 }
 
 function tableFor(event, journal) {
     var table = [0, 0, 0, 0];
     for (var i = 0; i < journal.length; i++) {
         var entry = journal[i], index = 0;
+        /*El if puede tener las sentencias sin que exista un bloque, cada sentencua puede estar
+        * separada por ; y no necesariamente estar dentro de {}*/
         if (hasEvent(event, entry)) index += 1;
         if (entry.squirrel) index += 2;
+        /*La asignación del valor a una posición del arreglo se hace directamente
+        arreglo[posicion] += valor. No se hace arreglo[posicion] = variable*/
+        /*El arreglo no se llena en orden. Se puede llenar al final la posición 0 del arreglo*/
         table[index] += 1;
     }
     return table;
@@ -19,9 +24,9 @@ function tableFor(event, journal) {
 /*archivo Journal bajado de internet: http://eloquentjavascript.net/code/jacques_journal.js*/
 var JOURNAL = [
     {"events":["carrot","pizza","weekend"],"squirrel":false},
-    {"events":["bread","ss","brushed teeth","weekend","touched tree"],"squirrel":false},
+    {"events":["bread","pizza","brushed teeth","weekend","touched tree"],"squirrel":true},
     {"events":["carrot","xx","brushed teeth","cycling","weekend"],"squirrel":false},
     {"events":["brussel sprouts","ice cream","brushed teeth","computer","weekend"],"squirrel":false},
-    {"events":["cauliflower","peanuts","brushed teeth","weekend"],"squirrel":false}
+    {"events":["cauliflower","peanuts","brushed teeth","weekend"],"squirrel":true}
 ];
 console.log(tableFor("pizza", JOURNAL));
