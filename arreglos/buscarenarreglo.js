3cc6d42f783592d8f3799f26483ed9980451f328
Juan Carlos Rojas <jc.rojas1@uniandes.edu.co>
2017-06-12 17:34:03 -0500

    JavaScript provides a loop construct specifically for going over the properties of an object. It looks a little like a normal for loop but distinguishes itself by the use of the word in.

---
+++ arreglos/buscarenarreglo.js
@@ -0,0 +1,18 @@
+/**
+ * Created by jcrojas on 12/06/2017.
+ */
+var map = {};
+function storePhi(event, phi) {
+    map[event] = phi;
+}
+
+storePhi("pizza", 0.069);
+storePhi("touched tree", -0.081);
+console.log("pizza" in map);
+// → true
+console.log(map["touched tree"]);
+// → -0.081
+
+for (var event in map)
+    console.log("The correlation for '" + event +
+        "' is " + map[event]);
